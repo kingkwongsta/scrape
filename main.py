fed93452aa50c63278416478c6b91e5c84dac4c7
Kwongster <bryant.j.kwong@gmail.com>
2024-06-02 18:39:14 -0700

    Add scrape functionality with playwright for Pokemon TCG website

---
+++ main.py
@@ -0,0 +1,14 @@
+# .venv\Scripts\activate
+
+import asyncio
+import pprint
+# from scrape import ascrape_playwright
+
+if __name__ == "__main__":
+    token_limit = 16385
+    url="https://www.tcgplayer.com/search/pokemon/sv-scarlet-and-violet-151?view=grid&productLineName=pokemon&setName=sv-scarlet-and-violet-151&ProductTypeName=Cards&page=1"
+    
+    async def scrape_with_playwright():
+        html_content = await ascrape_playwright(url, tags)
+
+    asyncio.run(scrape_with_playwright())
---
+++ scrape.py
@@ -0,0 +1,4 @@
+import asyncio
+import pprint
+
+from playwright import ascrape_playwright
