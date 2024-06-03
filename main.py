8ff7d72af2b1f11e2e7f46e102e277278a38804a
Kwongster <bryant.j.kwong@gmail.com>
2024-06-02 20:16:53 -0700

    Add extract function for content extraction with LLM model

--- main.py
+++ main.py
@@ -3,14 +3,24 @@
 
 import asyncio
 import pprint
-# from scrape import ascrape_playwright
+
+from ai_extractor import extract
+from scrape import ascrape_playwright
 
 if __name__ == "__main__":
     token_limit = 16385
     url="https://www.tcgplayer.com/search/pokemon/sv-scarlet-and-violet-151?view=grid&productLineName=pokemon&setName=sv-scarlet-and-violet-151&ProductTypeName=Cards&page=1"
     
-    async def scrape_with_playwright():
+    async def scrape_with_playwright(url: str, tags, **kwargs):
         html_content = await ascrape_playwright(url, tags)
+        print("Extracting content with LLM")
 
+        html_content_fits_context_window_llm = html_content[:token_limit]
+        extracted_content = extract(**kwargs,
+                                    content=html_content_fits_context_window_llm)
+
+        pprint.pprint(extracted_content)
+
+        
     asyncio.run(scrape_with_playwright())
     
